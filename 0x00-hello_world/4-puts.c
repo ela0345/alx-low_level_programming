3d428a93979b2a676445e07be2acec2161f63e0f
ela0345 <ela124512@example.com>
2022-06-02 13:36:37 -0700

    puts variable

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  * 4-puts.c
+  */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
