32b86d6e578184f43fa7fe5de65326b411436b1c
ela0345 <ela124512@example.com>
2022-06-03 03:39:55 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints letters alphabetically
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c<=z; c++)
+	{
+		putchar("%c\n", c);
+	}
+
+	return (0);
+}
