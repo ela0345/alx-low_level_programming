22af34190881c54f8d0c7dc2a2730b7715bb099f
ela0345 <ela124512@example.com>
2022-06-03 05:32:38 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n = '0'; n <= '9'; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
