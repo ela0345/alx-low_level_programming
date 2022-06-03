b0944e1754d36584d43d61f610f8f81da6c54400
ela0345 <ela124512@example.com>
2022-06-03 06:56:39 -0700

    nested loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers
+ * 
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c, x;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			putchar(c);
+			putchar(x);
+
+			if (c != '9' || i !='9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
