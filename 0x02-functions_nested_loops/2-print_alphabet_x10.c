8ed0a74b35e098ebb9901e4cfe0aabb83cd03402
ela0345 <ela124512@gmail.com>
2022-06-07 00:38:25 +0000

    nested for loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,16 @@
 
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- *
- * Return: 0
  */
 void print_alphabet_x10(void)
 {
-	for (int a = 0; a < 10; a++)
-	{
-		for (int b = 'a'; b <= 'z'; b++)
-		{
-			_putchar(b);
-		}
+	int count = 0;
+	char letter;
 
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
-
-
