e8ec15593989678c6c43e3c55f714ccd9b09569b
ela0345 <ela124512@gmail.com>
2022-06-07 00:27:31 +0000

    nested for loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * main - Print alphabet ten times using function 
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	int a, b;
+	
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 'a'; b <= 'z'; b++)
 		{
 			_putchar(b);
 		}
+
+		_putchar('\n');
 	}
 
-	_putchar('\n');
+	putchar('\n');
+
 }
 
 
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 a program that prints _putchar, followed by a new line
 call a fuction from print_alphabet from main.h and print letters alphabetically using _putchar
+nested for loop for printing alphabet 10 times
